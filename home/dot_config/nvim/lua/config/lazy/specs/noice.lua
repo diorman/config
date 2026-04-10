4222f4df9e05046d77fcf9a482800ecffa208328
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2026-04-09 21:54:37 -0700

    neovim: Add noice plugin

---
+++ home/dot_config/nvim/lua/config/lazy/specs/noice.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      notify = {
+        enabled = true,
+        view = "notify",
+      },
+      lsp = {
+        -- override markdown rendering so that cmp and other plugins use Treesitter
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        progress = {
+          enabled = false, -- fidget.nvim handles this
+        },
+      },
+      presets = {
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = true,        -- add a border to hover docs and signature help
+      },
+    },
+    keys = {
+      { "<leader>sn", "<cmd>Noice telescope<cr>", desc = "Search notifications" },
+    },
+  },
+}
