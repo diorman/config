c8e9b5146ca7ddc66fb9b2654658b98f3365e833
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2026-04-09 22:17:20 -0700

    neovim: Fix treesitter config

--- home/dot_config/nvim/lua/config/lazy/specs/treesitter.lua
+++ home/dot_config/nvim/lua/config/lazy/specs/treesitter.lua
@@ -2,7 +2,7 @@ return {
   { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs", -- Sets main module to use for opts
+    lazy = false,
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
